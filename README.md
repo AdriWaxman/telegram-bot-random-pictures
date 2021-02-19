8b4ff32e9b0fa53931cb0f968e58b83f3ffb7886
AdriMan <adrimpartner@gmail.com>
2021-02-20 00:01:24 +0100

    updates, created Readme.md

---
+++ README.md
@@ -0,0 +1,27 @@
+This is a bot for telegram build with node.js and the amazing library [node-telegram-bot-api](https://github.com/yagop/node-telegram-bot-api).
+
+Use commands in telegram to get a random picture with extra captions.
+
+# INSTRUCTIONS
+
+1. Create a bot 
+
+Create a bot with @BotFather in Telegram App. [More info](https://core.telegram.org/) to generate your api KEY.
+
+2. Clone the project
+
+Clone the project with ```
+git clone ```
+
+3. Install the dependencies
+```javascript
+npm install --save node-telegram-bot-api
+```
+4. Launch!
+
+Launch the project with:
+```javascript
+node bot.js
+```
+
+You can change "bot.js" in package.json
--- bot.js
+++ bot.js
@@ -2,72 +2,40 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = '1548860141:AAHOmadJFCYLoGAhuc9q7WncLX60B0fca7U';
 const bot = new TelegramBot(token, {polling:true});
+// const fs = require('fs');
+const data = require('./db/data.json').data;
+
+
 
 
 bot.on('polling_error', function(error){
     console.log(error);
 });
 
+
+
 bot.onText(/^\/start/, function(msg){
     var chatId = msg.chat.id;
     var nameUser = msg.from.first_name;
     
-    bot.sendMessage(chatId, "Bienvenido a mi bot " + nameUser);
+    bot.sendMessage(chatId, `Bienvenido a mi bot ${nameUser}`);
 });
 
 
-bot.onText(/^\/chatid/, function (msg) {
-  const chatId = msg.chat.id;
-  const myId = msg.from.id;
-  bot.sendMessage(chatId, 'El id del chat es: ' + chatId);
-});
-
-const apiImg = {
-  imgUrl1:"./resources/img/2008-09-18-15-41-14.jpg",
-  imgUrl2:"./resources/img/2008-09-18-21-19-22.jpg",
-  imgUrl3:"./resources/img/2008-11-19-21-45-14.jpg",
-  imgUrl4:"./resources/img/2008-11-22-18-37-01.jpg",
-  imgUrl5:"./resources/img/2008-12-17-23-03-45.jpg",
-  imgUrl6:"./resources/img/2009-01-07-01-57-18.jpg",
-
-};
-
-// const randomImg = (apiImg) =>{
-//   let keys = Object.keys(apiImg);
-//   return apiImg[keys[ keys.length * Math.random() << 0]]
-// };
 
 bot.onText(/^\/nostalgiapic/, (msg) =>{
   const chatId = msg.chat.id;
+  let randomId = Math.floor(Math.random()*data.length);
+  let randomImg = data[randomId].photo_id;
+  let arrComments = data[randomId].comments;
+  console.log(arrComments[0].text);
+  const photo = './resources/img/' + randomImg;
+  const photoText = '<i>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam eos inventore enim odit voluptatum asperiores dolores repellendus veniam architecto delectus?</i> \n <b>pene</b>';
   
-  
-  const photo = bot.sendPhoto(chatId, './resources/img/2008-09-18-21-19-22.jpg');
-  if(photo){
-    bot.sendMessage(chatId, 'eyuu');
-  }
+
+  bot.sendPhoto(chatId, photo, {caption: photoText, parse_mode:'HTML'});
+ 
 });
 
-// bot.onText(/^\/clean/, (msg) => {
-//   var chatId = msg.chat.id;
-//   var messageId = msg.message_id;
-  
-//   if (msg.reply_to_message == undefined){
-//       return; 
-//   }
-  
-//   bot.deleteMessage(chatId, messageId);
-// });
 
-bot.onText(/^\/ping/, function(msg){
-  var chatId = msg.chat.id;
-  var tipoChat = msg.chat.type;
-  
-  if (tipoChat == 'private'){
-      bot.sendMessage(chatId, "Pong!")
-  } 
-  
-  else if (tipoChat == 'supergroup') {
-      bot.sendMessage(chatId, "Este comando solo funciona en privado")
-  }
-});
 
