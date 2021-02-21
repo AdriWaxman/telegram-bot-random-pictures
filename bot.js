74b7008c536a265c2e693481a35902e68fa41c1e
roblesalfredo <37293733+roblesalfredo@users.noreply.github.com>
2021-02-21 21:56:57 +0100

    Added 5 comments and UTF-8 decoding

--- bot.js
+++ bot.js
@@ -6,12 +6,9 @@ const bot = new TelegramBot(token, {polling:true});
 const data = require('./db/data.json').data;
 
 
-
-
 bot.on('polling_error', function(error){
     console.log(error);
 });
-
 
 
 bot.onText(/^\/start/, function(msg){
@@ -22,19 +19,24 @@ bot.onText(/^\/start/, function(msg){
 });
 
 
-
 bot.onText(/^\/nostalgiapic/, (msg) =>{
   const chatId = msg.chat.id;
   let randomId = Math.floor(Math.random()*data.length);
   let randomImg = data[randomId].photo_id;
   let arrComments = data[randomId].comments;
-  console.log(arrComments[0].text);
   const photo = './resources/img/' + randomImg;
-  const photoText = '<i>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam eos inventore enim odit voluptatum asperiores dolores repellendus veniam architecto delectus?</i> \n <b>pene</b>';
-  
 
+  var text = '';
+  const l = arrComments.slice(0, 5).length;
+  for (let index = 0; index < l; index++) {
+    const date = arrComments[index].date;
+    var username = Buffer.from(arrComments[index].username, 'utf-8');
+    var comment = Buffer.from(arrComments[index].text, 'utf-8');
+    text += '<b>' + date + ' - ' + username + '</b>\n' + comment + '\n\n';
+  }
+
+  const photoText = text;
   bot.sendPhoto(chatId, photo, {caption: photoText, parse_mode:'HTML'});
- 
 });
 
 
