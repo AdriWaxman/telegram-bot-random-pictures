cb23d185559252cb41eb570d806cd636273f9854
AdriMan <adrimpartner@gmail.com>
2021-02-22 12:49:57 +0100

    Modificaciones en botjs

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const token = '1548860141:AAHOmadJFCYLoGAhuc9q7WncLX60B0fca7U';
 const bot = new TelegramBot(token, {polling:true});
 // const fs = require('fs');
 const data = require('./db/data.json').data;
-
+let RANDOM_ID = Math.floor(Math.random()*data.length);
 
 bot.on('polling_error', function(error){
     console.log(error);
@@ -13,28 +13,35 @@ bot.on('polling_error', function(error){
 
 bot.onText(/^\/start/, function(msg){
     var chatId = msg.chat.id;
-    var nameUser = msg.from.first_name;
-    
-    bot.sendMessage(chatId, `Bienvenido a mi bot ${nameUser}`);
+    // var nameUser = msg.from.first_name;
+    let sticker = './resources/stickers/sticker-times.webp'
+    let text = `<b>What a chesnut!...</b> \n
+                 Esto serás lo que dirás cuando veas el viaje que te espera. \n
+                 Cómo diría el sabio Víctor, fotos no que capturan el alma, y aquí hay mucha alma e historias. \n
+                 Si nuncas has viajado en el tiempo, no te preocupes las primeras veces sentiras nauseas y mucha vergüenza ajena. \n
+                 !Disfrutad y consumid con moderación!😎🚀 
+                `;
+    bot.sendMessage(chatId, text, { parse_mode: 'HTML'});
+    bot.sendSticker(chatId, sticker, {caption: text, parse_mode: 'HTML'});
 });
 
 
-bot.onText(/^\/nostalgiapic/, (msg) =>{
+bot.onText(/^\/travelphotomachine/, (msg) =>{
   const chatId = msg.chat.id;
-  let randomId = Math.floor(Math.random()*data.length);
-  let randomImg = data[randomId].photo_id;
-  let arrComments = data[randomId].comments;
+  
+  let randomImg = data[RANDOM_ID].photo_id;
+  let arrComments = data[RANDOM_ID].comments;
   const photo = './resources/img/' + randomImg;
 
-  var text = '';
+  let text = '';
   const l = arrComments.slice(0, 5).length;
-  for (let index = 0; index < l; index++) {
-    const date = arrComments[index].date;
-    var username = Buffer.from(arrComments[index].username, 'utf-8');
-    var comment = Buffer.from(arrComments[index].text, 'utf-8');
+  for (let i = 0; i < l; i++) {
+    const date = arrComments[i].date;
+    let username = Buffer.from(arrComments[i].username, 'utf-8');
+    let comment = Buffer.from(arrComments[i].text, 'utf-8');
     text += '<b>' + date + ' - ' + username + '</b>\n' + comment + '\n\n';
   }
-
+ 
   const photoText = text;
   bot.sendPhoto(chatId, photo, {caption: photoText, parse_mode:'HTML'});
 });
